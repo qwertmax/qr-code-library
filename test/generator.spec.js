4192e2bb421aa5ea635e81d69eb5d9e08493850f
akondyuruna <alexandra.kondyurina@burningbuttons.com>
2019-10-30 16:39:26 +0600

    Add test for generator

---
+++ test/generator.spec.js
@@ -0,0 +1,15 @@
+const generator = require('../lib/generator');
+
+test('getQRCodeBitMatrix return correct bit matrix', () => {
+  const payload = 'text';
+  const bitMatrix = generator.getQRCodeBitMatrix(payload, {});
+
+  expect(bitMatrix.size).toBeGreaterThan(0);
+  expect(bitMatrix.data).toBeInstanceOf(Array);
+});
+
+test('getQRCodeBitMatrix can work with default options', () => {
+  expect(() => {
+    generator.getQRCodeBitMatrix('text', {});
+  }).not.toThrow();
+});
