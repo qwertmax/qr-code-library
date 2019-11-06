032c31fcb7e2129cf1cce3acb4475aa4a7b682a2
akondyuruna <alexandra.kondyurina@burningbuttons.com>
2019-11-06 17:45:26 +0600

    Add converters tests

--- src/converters.js
+++ src/converters.js
@@ -21,6 +21,7 @@ export const convertToVCard = ({
     `N:${lastName};${firstName}`,
     `FN: ${firstName} ${lastName}`,
     organization ? `ORG:${organization}` : null,
+    role ? `TITLE: ${role}` : null,
     phone ? `TEL;TYPE=work,voice:${phone}` : null,
     `ADR;TYPE=WORK:${[
       '',
@@ -32,7 +33,6 @@ export const convertToVCard = ({
       country || ''
     ].join(';')}`,
     email ? `EMAIL:${email}` : null,
-    role ? `TITLE: ${role}` : null,
     url ? `URL:${url}` : null,
     'END:VCARD\r\n'
   ]
@@ -45,10 +45,10 @@ export const convertToVCard = ({
 export const convertToEmail = ({ email, subject, body, cc, bcc }) => {
   const emailUri = `mailto:${email}`;
   const query = [
-    cc ? `cc=${cc}` : null,
-    bcc ? `bcc=${bcc}` : null,
     subject ? `&subject=${subject}` : null,
-    body ? `body=${body}` : null
+    body ? `body=${body}` : null,
+    cc ? `cc=${cc}` : null,
+    bcc ? `bcc=${bcc}` : null
   ].filter(v => v !== null);
 
   if (query.length) {
